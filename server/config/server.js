8289f2f7e1770d1795ac5b30b858f389a8390c60
Vinicius <vviniciuss@gmail.com>
2019-09-28 18:01:10 -0300

    Criação da API: curso e contato

---
+++ server/config/server.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const allowCors = require('./cors')
+
+const port = 3200;
+
+//middlewares - singletons: commons js
+const server = express(); //novo servidor
+
+//para toda requisição que chegar, use o bodyparser para
+//interpretar chegadas no formato urlencoded
+server.use(bodyParser.urlencoded({ extended: true }))
+
+//considera o formato json no corpo da requisição
+server.use(bodyParser.json());
+
+server.use(allowCors);
+
+const callback = function () {
+ //template string (observe a crase)
+ console.log(`servidor no ar, na porta ${port}`);
+}
+
+server.listen(port, callback);
+
+module.exports = server;
---
+++ server/loader.js
@@ -0,0 +1,4 @@
+require('./config/db');
+const server = require('./config/server');
+const routes = require('./config/routes');
+routes(server);
